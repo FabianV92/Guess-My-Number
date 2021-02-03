e75fa9693c7913178020f06afb738bd91dd1290d
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-02-03 14:16:44 +0100

    Added some code

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 'use strict';
-//declaring and initializing variables
+// Declaring and initializing variables
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let messageToUser = document.querySelector(".message");
 let currentScore = 20;
 let highScore = 0;
 
-//check button
+// Adding selectors
+const helpTxt = document.querySelector(".layer")
+const helpBtn = document.querySelector(".help-button");
+
+// Implementing the help button
+helpBtn.addEventListener("click", function () {
+    helpTxt.classList.toggle("hidden");
+})
+// Implementing the ESC press to close help txt
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape")
+        helpTxt.classList.add("hidden");
+})
+
+
+// Check button
 document.querySelector(".check").addEventListener
 ("click", function () {
     const input = Number(document.querySelector(".guess").value);
-    //if checked button pressed with an empty value output: Not a Number
+    // If checked button pressed with an empty value output: Not a Number
     if (!input) {
         messageToUser.textContent = "Not a Number !";
     } else if (input !== secretNumber) {
-        //subtraction of score
+        // Subtraction of score
         if (currentScore > 0) {
             currentScore--;
             document.querySelector(".score").textContent = "Score :" + currentScore;
         }
-        //checking and output of: You have lost/number too low/number too high
+        // Checking and output of: You have lost/number too low/number too high
         if (input < secretNumber || input > secretNumber) {
             messageToUser.textContent = (currentScore < 1) ?
                 "You have lost!" : (input < secretNumber) ? "Number is too low !" : "Number is too high !";
         }
     }
-    //if correct number
+    // If correct number
     if (input === secretNumber) {
         messageToUser.textContent = "Congratulation, you guessed the right number !";
         if (currentScore > highScore) {
@@ -37,7 +52,7 @@ document.querySelector(".check").addEventListener
         document.querySelector(".mid-big-number").textContent = secretNumber;
     }
 })
-//implementing the again button and resetting the values of the page /score/background-color/GuessNumber/SecretNumber
+// Implementing the again button and resetting the values of the page /score/background-color/GuessNumber/SecretNumber
 document.querySelector(".again").addEventListener
 ("click", function () {
     secretNumber = Math.trunc(Math.random() * 10) + 1;
